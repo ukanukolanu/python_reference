ba9202e54ff947e53cb7a08dc61d5838d2fe5721
ukanukolanu <ukanukolanu@192.168.0.194>
2014-07-14 22:58:35 -0700

    Added method for finding unique elements in a list.

--- tutorials/hello_world.py
+++ tutorials/hello_world.py
@@ -1,3 +1,26 @@
 def hello_world():
     """This is a hello world example function."""
     print('Hello, World!')
+    
+def find_unique_elements(list_input,sort=True):
+    """This method finds the unique elements of a list (or array in PERL), sorts and returns it
+    arg list_input:The list from which the unique elements have to be identified. 
+    type list_input:list of integers
+    arg sort: Set to true if the unqiue list has to be sorted. False otherwise.
+    type sort: Boolean
+    rtype:sorted list of unique elements.
+    """
+    #define the empty list that holds the unique elements
+    unique_elements = []
+    #Loop through the list
+    for element in list_input:
+        #If the element is already present in the unique list, then skip. Otherwise add it.
+        if element not in unique_elements:
+            unique_elements.append(element)
+    if sort:
+        return sorted(unique_elements)
+    else:
+        return unique_elements
+
+print "As-is list is", find_unique_elements(list_input = [1,3,12,8,3,4,8,3,], sort = False)
+print "Sorted list is", find_unique_elements(list_input = [1,3,12,8,3,4,8,3,], sort = True)
